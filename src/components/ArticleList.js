2083179ca25f6201990422eb5717af66e0cb993d
Kush <kuriaisac@gmail.com>
2024-08-03 15:16:56 +0300

    first commit

---
+++ src/components/ArticleList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}) {
+    console.log(posts)
+
+// const displayPosts = posts.map((post) => {
+//     return (
+//         <Article key={post.id} title={post.title} date={post.date} minutes={post.minutes}preview={post.preview}/>
+//     )
+// })
+    return (
+    
+        <main>
+            {posts.map((post) => {
+                return  <Article key={post.id} title={post.title} date={post.date} minutes={post.minutes} preview={post.preview}/>})}
+
+        </main>
+
+    )
+}
+export default ArticleList
